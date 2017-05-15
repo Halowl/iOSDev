e694ba221e9e63448524915dfef88af3330fe666
dxaandpl <271584255@qq.com>
2017-05-15 17:50:45 +0800

    判断iPhone 尺寸

--- Demo/Transition/Transition/App.h
+++ Demo/Transition/Transition/App.h
@@ -9,5 +9,16 @@
 #ifndef App_h
 #define App_h
 
+#define IS_iPHONE4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960),[[UIScreen mainScreen] currentMode].size): NO)
+#define IS_iPHONE5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136),[[UIScreen mainScreen] currentMode].size): NO)
+#define IS_iPHONE6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(750, 1334),[[UIScreen mainScreen] currentMode].size)): NO)
+#define IS_iPHONE6P ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? (CGSizeEqualToSize(CGSizeMake(1242, 2208),[[UIScreen mainScreen] currentMode].size) || CGSizeEqualToSize(CGSizeMake(1125, 2001),[[UIScreen mainScreen] currentMode].size)): NO)
+
+/*
+ //.h
+ extern NSString *const SureShouldShowGuide;
+ //.m
+ NSString *const SureShouldShowGuide = @"SureShouldShowGuide";
+*/
 
 #endif /* App_h */
